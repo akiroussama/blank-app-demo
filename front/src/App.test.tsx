32a35eff71e2e67c22656b4d99909330a4899f98
oakir <oakir@ecomundo.eu>
2022-12-09 08:55:37 +0100

    install react testing library & add real simple test

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,4 +1,6 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect, test } from 'vitest';
+import {render, screen} from '@testing-library/react';
+import App from './App';
 
 describe('something truthy and falsy', () => {
   it('true to be true', () => {
@@ -9,3 +11,18 @@ describe('something truthy and falsy', () => {
     expect(false).toBe(false);
   });
 });
+
+describe("Real test", () => {
+    test("should show count element", () => {
+        
+        render(<App></App>);
+
+        expect(screen.getByText(/count is/i)).toBeDefined()
+    });
+    test("should show count element", () => {
+        
+        render(<App></App>);
+
+        expect(screen.getByText(/count is/i)).toBeDefined()
+    })
+})
