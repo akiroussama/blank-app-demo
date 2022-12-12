1d010dbade1a88485b79a84ce6da2f1b90e5f1e9
oakir <oakir@ecomundo.eu>
2022-12-12 06:57:17 +0100

    improve look and feel welcome page

--- front/src/components/Welcome.tsx
+++ front/src/components/Welcome.tsx
@@ -9,7 +9,7 @@ interface WelcomeProps {
 const Welcome: React.FC<WelcomeProps> = ({username, onLogout}) => {
   return (
     <div className="max-w-screen-sm p-12 mx-auto bg-gray-50 rounded-md shadow-lg">
-      <h1 className="text-2xl bg-purple-900">Welcome {username}!</h1>
+      <h1 className="text-2xl">Welcome {username}!</h1>
       <Button className='mt-2' onClick={onLogout}>Log Out</Button>
     </div>
   );
